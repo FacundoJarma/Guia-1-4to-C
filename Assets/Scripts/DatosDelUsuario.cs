9eb1de545b72ad71698b2ec76e5ebb42883ed5fc
FacundoJarma <49127963@est.ort.edu.ar>
2025-04-01 08:44:00 -0300

    Ejercicio 1 Terminado

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    [SerializeField] string nombre;
+    [SerializeField] int edad;
+    [SerializeField] float estatura;
+    [SerializeField] bool dona;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombre + " ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -8,11 +8,21 @@ public class DeclaracionDeVariables : MonoBehaviour
     float kilosVendidos;
     string nombreAlumno;
     bool estaAprobado;
-
+        
     // Start is called before the first frame update
     void Start()
     {
+        cantidadAlumnos = 33;
+        kilosVendidos = 23.1f;
+        nombreAlumno = "Facundo";
+        estaAprobado = true;
+
         
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(kilosVendidos);
+        Debug.Log(nombreAlumno);
+        Debug.Log(estaAprobado);
+
     }
 
     // Update is called once per frame
